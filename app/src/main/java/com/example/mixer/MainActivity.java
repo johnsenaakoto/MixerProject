3b3bba5b427e438a57134fa7d699588079da5e37
John Akoto <johnsenaakoto@gmail.com>
2022-03-25 12:36:47 -0500

    Commit with Fragments and Nav Bar

--- app/src/main/java/com/example/mixer/MainActivity.java
+++ app/src/main/java/com/example/mixer/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.mixer;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -11,6 +16,9 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.mixer.adapters.DrinkAdapter;
+import com.example.mixer.fragments.FavoritesFragment;
+import com.example.mixer.fragments.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,81 +31,36 @@ import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Create string to hold http for API request
-    public static final String RANDOM_DRINK_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
-
-    // Create a tag for logging this activity
-    public static final String TAG = "MainActivity";
-    List<Drink> drinks;
-    private SwipeRefreshLayout swipeContainer;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().hide();   // Hide Action bar
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView rvDrinks = findViewById(R.id.rvDrinks);
-        drinks = new ArrayList<>();
 
-        // Create the adapter
-        DrinkAdapter drinkAdapter = new DrinkAdapter(this, drinks);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
 
-        // Set the adapter o the recycler view
-        rvDrinks.setAdapter(drinkAdapter);
-
-        // Set a layout manager
-        rvDrinks.setLayoutManager(new LinearLayoutManager(this));
-        queryDrinks(drinkAdapter);
-
-        // Refresh screen when you swipe up
-        swipeContainer = findViewById(R.id.swipeContainer);
-        // Configure the refreshing colors
-        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
-
-        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onRefresh() {
-                Log.i(TAG, "fetching new data!");
-                drinks.clear();
-                drinkAdapter.notifyDataSetChanged();
-                queryDrinks(drinkAdapter);
-                swipeContainer.setRefreshing(false);
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.action_favorites:
+                    default:
+                        // TODO: build Favorites Fragment
+                        fragment = new FavoritesFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
             }
         });
-    }
-
-    private void queryDrinks(DrinkAdapter drinkAdapter) {
-        for (int i = 0; i < 10; i++) {
-            // Instantiate an AsyncHttpClient to execute the API request
-            AsyncHttpClient client = new AsyncHttpClient();
-            final Drink[] drink = new Drink[1];
-
-            // Make a get request on the client object
-            client.get(RANDOM_DRINK_URL, new JsonHttpResponseHandler() {
-                @Override
-                public void onSuccess(int statusCode, Headers headers, JSON json) {
-                    Log.d(TAG, "Success"); // log for success, connected to TAG
-                    JSONObject jsonObject = json.jsonObject; // we store the response jsonObject in the variable jsonObject
-                    try {
-                        JSONArray result = jsonObject.getJSONArray("drinks");
-                        Log.i(TAG, "Results: " + result.toString()); //logs onSuccess and shows what is in results
-                        drink[0] = Drink.fromJsonArray(result);
-                        drinks.add(Drink.fromJsonArray(result));
-                        drinkAdapter.notifyDataSetChanged();
-                        Log.i(TAG, "Drinks is: " + drinks);
-                    } catch (JSONException e) {
-                        Log.e(TAG, "Hit json exception", e); // handles the exception if results is not in the jsonObject
-                    }
-                }
-                @Override
-                public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                    Log.d(TAG, "Failure: " + response);
-                }
-            });
-
-        }
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
