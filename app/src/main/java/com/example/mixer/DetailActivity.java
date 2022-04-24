9ffcf68be79db12ec662f9f97092175233fd9f2e
kdevlin01 <karldevlin01@gmail.com>
2022-04-24 13:45:12 -0500

    Undo snack bar

--- app/src/main/java/com/example/mixer/DetailActivity.java
+++ app/src/main/java/com/example/mixer/DetailActivity.java
@@ -158,7 +158,7 @@ public class DetailActivity extends AppCompatActivity {
                         public void done(ParseException e) {
                             // inside done method checking if the error is null or not.
                             if (e == null) {
-//                                Toast.makeText(DetailActivity.this, "Favorite Removed..", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(DetailActivity.this, "Favorite Removed..", Toast.LENGTH_SHORT).show();
                             } else {
                                 Toast.makeText(DetailActivity.this, "Failed to remove Favorite..", Toast.LENGTH_SHORT).show();
                             }
